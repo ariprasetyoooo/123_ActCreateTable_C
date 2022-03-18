b74441922953f9be038ee5f6d1ad804c048ed752
AriPrasetyo1 <prasetyoari600@gmail.com>
2022-03-18 10:22:56 +0700

    Add project files.

---
+++ Create Tabel/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace Create_Tabel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new Program().Create();
+        }
+
+        public void  Create()
+        {
+            SqlConnection con = null;
+            try
+            {
+                con = new SqlConnection("data source=LAPTOP-2M5MKTU9;database=ProdiTI;Integrated Security = TRUE");
+                con.Open();
+                SqlCommand cm = new SqlCommand("create table mahasiswa_baru (NIM char(12) not null primary key, Nama Varchar(50), Alamat Varchar(255),Jenis_kelamin char(1))", con);
+                cm.ExecuteNonQuery();
+
+                Console.WriteLine("Table sukses dibuat!");
+                Console.ReadKey();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Oops, sepertinya ada yang salah." + e);
+                Console.ReadKey();
+            }
+            finally
+            {
+                con.Close();
+            }
+        }
+    }
+}
